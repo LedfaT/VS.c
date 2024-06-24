ad0d4e90ae7f65c2914cc6081a0aa682675ba3d6
David Zaitsev <zaitsevdavid4@gmail.com>
2024-06-25 01:24:30 +0300

    add opacity to navigation

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const btnsOpen = document.querySelectorAll(".arrow");
+const nav = document.querySelector(".nav");
+
 console.log(btnsOpen);
 btnsOpen.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
@@ -8,3 +10,18 @@ btnsOpen.forEach(function (btn) {
     targ.style.transform = "rotate(180deg)";
   });
 });
+
+const HandleHover = function (e, opacity) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("a");
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener("mouseover", HandleHover.bind(0.5));
+nav.addEventListener("mouseout", HandleHover.bind(1));
