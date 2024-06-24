3584adf27cd99c46178999b98f3703c8b5960297
David Zaitsev <zaitsevdavid4@gmail.com>
2024-06-25 01:11:55 +0300

    add popup

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const btnsOpen = document.querySelectorAll(".arrow");
 console.log(btnsOpen);
+btnsOpen.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const targ = e.target;
+    const data = e.target.closest(".code__info").dataset.ex;
+    document.querySelector(`.exemple--${data}`).classList.remove("hidden");
+    targ.style.transform = "rotate(180deg)";
+  });
+});
